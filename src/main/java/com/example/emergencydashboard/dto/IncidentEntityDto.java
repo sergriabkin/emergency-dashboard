6575ab5b8601fca8ddadd70acce315742b3094dc
sergriabkin <sergriabkin@gmail.com>
2024-03-02 17:13:25 +0300

    add README + use generated uuid in id fields

--- src/main/java/com/example/emergencydashboard/dto/IncidentEntityDto.java
+++ src/main/java/com/example/emergencydashboard/dto/IncidentEntityDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class IncidentEntityDto {
 
-    private Integer id;
+    private String id;
     private String incidentType;
     private double latitude;
     private double longitude;
