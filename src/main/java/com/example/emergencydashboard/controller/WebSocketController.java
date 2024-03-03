bdee30b263510bf74343c30d8022aef1fc721289
Sergey Riabkin <sergriabkin@gmail.com>
2024-03-03 10:25:16 +0300

    basic form for saving and broadcasting incidents (#2)
    * implement basic form (thymeleaf) for saving and broadcasting incidents to all opened clients (websocket)
    
    * show latitude and longitude on a page
    
    * Delete .DS_Store

---
+++ src/main/java/com/example/emergencydashboard/controller/WebSocketController.java
@@ -0,0 +1,28 @@
+package com.example.emergencydashboard.controller;
+
+import com.example.emergencydashboard.dto.IncidentEntityDto;
+import com.example.emergencydashboard.service.IncidentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class WebSocketController {
+    private static final String INCIDENT_SAVED_TEMPLATE = "incident {} is successfully saved and gonna broadcast to all subscribers";
+    private static final String RECEIVED_INCIDENT_TEMPLATE = "Received incident: {}";
+
+    private final IncidentService service;
+
+    @MessageMapping("/incident")
+    @SendTo("/topic/incidents")
+    public IncidentEntityDto notifyIncident(IncidentEntityDto incident) {
+        log.info(RECEIVED_INCIDENT_TEMPLATE, incident);
+        IncidentEntityDto savedIncident = service.saveIncident(incident);
+        log.info(INCIDENT_SAVED_TEMPLATE, savedIncident.getId());
+        return savedIncident;
+    }
+}
