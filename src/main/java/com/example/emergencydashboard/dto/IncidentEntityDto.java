d76db8ee241239161dfd55469e73c6d71f1f135d
sergriabkin <sergriabkin@gmail.com>
2024-03-03 17:02:21 +0300

    add IncidentRestControllerTest

--- src/main/java/com/example/emergencydashboard/dto/IncidentEntityDto.java
+++ src/main/java/com/example/emergencydashboard/dto/IncidentEntityDto.java
@@ -12,15 +12,18 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class IncidentEntityDto {
 
+    public static final String LONGITUDE_RANGE_MESSAGE = "Longitude must be between -180.0 and 180.0";
+    public static final String LATITUDE_RANGE_MESSAGE = "Latitude must be between -90.0 and 90.0";
+
     private String id;
     private String incidentType;
 
-    @DecimalMin(value = "-90.0", message = "Latitude must be between -90.0 and 90.0")
-    @DecimalMax(value = "90.0", message = "Latitude must be between -90.0 and 90.0")
+    @DecimalMin(value = "-90.0", message = LATITUDE_RANGE_MESSAGE)
+    @DecimalMax(value = "90.0", message = LATITUDE_RANGE_MESSAGE)
     private Double latitude;
 
-    @DecimalMin(value = "-180.0", message = "Longitude must be between -180.0 and 180.0")
-    @DecimalMax(value = "180.0", message = "Longitude must be between -180.0 and 180.0")
+    @DecimalMin(value = "-180.0", message = LONGITUDE_RANGE_MESSAGE)
+    @DecimalMax(value = "180.0", message = LONGITUDE_RANGE_MESSAGE)
     private Double longitude;
 
     private LocalDateTime timestamp;
